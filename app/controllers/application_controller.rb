5c1b32492e6b1eae37b9d4b1859de9fb868f4d0c
Justin Phillips <justinphil02@yahoo.com>
2020-02-09 00:02:48 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,25 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
+  get '/' do
+    erb :'/home'
+  end
+
+  get '/signup' do
+    erb :'/users/signup'
+  end
+
+  get '/login' do
+    erb :'/users/login'
+  end
+
+  helpers do
+    def logged_in?
+      !!current_user
+    end
+
+    def current_user
+      @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+    end
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,22 @@
 class UsersController < ApplicationController
 
 
+  get '/users/login' do
+    erb :'/users/login'
+  end
+
+  get '/users/signup' do
+    erb :'/users/signup'
+  end
+
+  post '/users/signup' do
+    user = User.new(params)
+    if user.save && !!params[:username]
+      session[:user_id] = user.id
+      redirect '/tweets'
+    else
+      redirect '/signup'
+    end
+  end
+
 end
